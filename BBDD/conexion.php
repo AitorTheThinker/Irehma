bae5d55ce8fdf69926aaf80c5a041fd74f9f2217
Aitor Nain Mendoza Vallejo <65561706+AitorTheThinker@users.noreply.github.com>
2022-05-06 10:03:41 +0200

    Add files via upload

---
+++ BBDD/conexion.php
@@ -0,0 +1,6 @@
+<?php
+$servidor="mysql-5706.dinaserver.com";
+$usuario="irehma";
+$password="A_mendoza699";
+$bbdd="irehma";
+?>
---
+++ BBDD/otros_servicios.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="icon" href="/img/logos/negro.png" type="image/gif" sizes="16x16">
+    <title>IREHMA</title>
+</head>
+<body>
+
+<body>  
+<div class="inside-top-bar grid-container grid-parent" style="background-color: #F7F0F0;"><aside id="custom_html-2" class="widget_text widget inner-padding widget_custom_html">
+    <div class="textwidget custom-html-widget"><p style="text-align:center">¿Piensas hacer una reforma en tu comunidad? <strong>Llama ahora al <a href="tel:+34676152810" rel="nofollow">676152810</a> o <a href="#contacta" rel="nofollow">contacta</a></div>
+    <nav class="navbar sticky-top navbar-expand-lg navbar-light " style="background-color: #DD9A00;"> 
+        <div class="container-fluid">
+          <a class="navbar-brand" href="../index.html"> <img  class="logo" src="../img/logos/blanco.png">
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="../index.html">Irehma</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#sobre_nosotros">Sobre nosotros</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./mostrar_reformas.php">Reformas</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./mostrar_proyectos.php">Proyectos</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./otros_servicios.php">Otros Servicios</a>
+              </li>
+              <!-- Presupuesto para Particulares y Empresas-->
+              <li class="nav-item dropdown">
+                <li class="nav-item">
+                        <a class="nav-link" href="../contacto/form_particular.html">Contacto</a>
+                </li>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+<!-- HERO -->
+<div class="hero vh-100 d-flex align-items-center" id="home">
+  <div class="container">
+      <div class="row">
+          <div class="col-lg-7 mx-auto text-center">
+              <h1 class="display-4"></h1>
+          </div>
+      </div>
+  </div>
+</div>
+<!--Fin del Hero -->
+ <!-- SERVICES, indicaremos nuestros servicios -->
+ <section id="services">
+  <div class="container">
+      <div class="row mb-5">
+          <div class="col-md-8 mx-auto text-center">
+              <h1  id="sobre_nosotros">Sobre nosotros</h1>
+              <p>SERVICIO “Llave en Mano”, que liberan al cliente de las GESTIONES Y TRÁMITES:</p>
+          </div>
+      </div>
+      <div class="row g-4">
+          <div class="col-lg-4 col-sm-6">
+              <div class="service card-effect bounceInUp">
+                  <div class="iconbox">
+                      <i class='bx bxs-check-shield'></i>
+                  </div>
+                  <h5 class="mt-4 mb-2">FINANCIACIÓN</h5>
+                  <p>Nuestros principales clientes son las Comunidades de Propietarios.  </p>
+              </div>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+              <div class="service card-effect">
+                  <div class="iconbox">
+                      <i class='bx bxs-comment-detail'></i>
+                  </div>
+                  <h5 class="mt-4 mb-2">HONORARIOS TÉCNICOS</h5>
+                  <p>Proyecto, Dirección de obra, cumplimiento de Seguridad y Salud… </p>
+              </div>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+              <div class="service card-effect">
+                  <div class="iconbox">
+                      <i class='bx bxs-cog'></i>
+                  </div>
+                  <h5 class="mt-4 mb-2">SUBVENCIONES</h5>
+                  <p>Experiencia en convocatorias anteriores y CONSTANCIA. </p>
+              </div>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+              <div class="service card-effect">
+                  <div class="iconbox">
+                      <i class='bx bxs-heart'></i>
+                  </div>
+                  <h5 class="mt-4 mb-2">LICENCIA Y PERMISOS OCUPACIÓN</h5>
+                  <p>Ante Ayuntamiento y/ó Junta Municipal correspondiente. </p>
+              </div>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+              <div class="service card-effect">
+                  <div class="iconbox">
+                      <i class='bx bxs-rocket'></i>
+                  </div>
+                  <h5 class="mt-4 mb-2">IREHMA ofrece</h5>
+                  <p>Equipo con EXPERIENCIA  de años en REHABILITACIÓN (Técnicos y Trabajadores cualificados)
+
+                    CUMPLIMIENTO  de  la CALIDAD ofrecida en presupuestos así como con  los PLAZOS de ejecución pactados.
+                    
+                    IMPLICACIÓN en nuestras GESTIONES de principio a fin. 
+                    
+                    INTERÉS en cada OBRA por conseguir el mejor resultado. Nuestra mejor publicidad son los CLIENTES SATISFECHOS. 
+                     </p>
+              </div>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+              <div class="service card-effect">
+                  <div class="iconbox">
+                      <i class='bx bxs-doughnut-chart'></i>
+                  </div>
+                  <h5 class="mt-4 mb-2">¡¡Conócenos!!</h5>
+                  <p>Empresa  Especializada en la Conservación  y Rehabilitación de Edificios</p>
+              </div>
+          </div>
+      </div>
+  </div>
+</section>
+<!-- SERVICES -->
+
+  <!--Fin del contenedor-->
+</div>
+<!-- INICIO DEL FOOTER-->
+<footer>
+  <div class="footer-top">
+      <div class="container">
+          <div class="row gy-4">
+              <div class="col-lg-4">
+                  <img class="logo" src="#" alt="">
+              </div>
+              <div class="col-lg-2">
+                  <h5 class="text-black">Marca</h5>
+                  <ul class="list-unstyled">
+                      <li><a href="#sobre_nosotros">Sobre nosotros</a></li>
+                      <li><a href="#">Servicios</a></li>
+                      <li><a href="#">Presupuesto</a></li>
+                  </ul>
+              </div>
+              <div class="col-lg-2">
+                  <h5 class="text-black">Más</h5>
+                  <ul class="list-unstyled">
+                      <li><a href="#">FAQ's</a></li>
+                      <li><a href="#">Política de privacidad </a></li>
+                      <li><a href="#">Shipment</a></li>
+                  </ul>
+              </div>
+              <div class="col-lg-4" id="contacta">
+                  <h5 class="text-black">Contact</h5>
+                  <ul class="list-unstyled">
+                      <li>Dirección: Pasaje las castellanas 1070</li>
+                      <li>Correo: irehma@gmail.com</li>
+                      <li>Teléfono: 676152810</li>
+                  </ul>
+              </div>
+          </div>
+      </div>
+  </div>
+  <div class="footer-bottom py-3">
+      <div class="container">
+          <div class="row">
+              <div class="col-md-6">
+                  <p class="mb-0">© 2022 copyright all right reserved | Designed by Aitor Mendoza <i
+                      class="bx bx-heart text-danger"></i>
+              </div>
+          </div>
+      </div>
+  </div>
+</footer>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
+<section id="contenedor">
+	<h1>Listado de reformas</h1>
+
+	<?php
+	// <?php levanta interprete de php.
+
+	/* Conectar archivo conexion a este archivo. (include('conexion_agenda.php')---Para conectar y si dar error sigue la ejecucion del programa.)*/
+	require('conexion.php');
+	// echo sirve para mostrar info por pantalla.
+	// 1.- Conexion.
+	if($conexion=mysqli_connect($servidor, $usuario, $password, $bbdd)){
+		// Entramos por aqui...
+		mysqli_query($conexion, "SET NAMES 'UTF8'");
+		// 2.- Seleccionamos la base de datos. 	
+		if(mysqli_select_db($conexion, $bbdd)){
+			// 3.- Definimos la query.
+			$consulta="SELECT * FROM reformas ORDER BY cod_reforma;";
+			// 4.- Ejecutar la query.
+			$resultado=mysqli_query($conexion, $consulta);
+			// 5.- Comprobar.
+			//Si hay algun error, lo muestro en caso contrario continuo con el WHILE.
+			if(mysqli_errno($conexion)!=0){
+				//Quiere decir que hay un error.
+				echo "<p>No error: ".mysqli_errno($conexion)."</p>";
+				echo "<p>Mensaje error: ".mysqli_error($conexion)."</p>";
+			} else{
+				//Quiere decir que no hay error.
+			}
+			// 6.- Mostrar datos por pantalla (Si es un query SELECT)
+
+			while($dato=mysqli_fetch_array($resultado)){
+				//echo "<p class='negrita'>".$dato["nombre"]." ".$dato["apellidos"]."</p>";
+
+				echo "<p><a href='mostrar_reformas.php?codigo=$dato[0]'>$dato[nombre]</a></p>";
+			} 
+		}
+        if(mysqli_select_db($conexion, $bbdd)){
+			// 3.- Definimos la query.
+			$consulta="SELECT * FROM fotos ORDER BY cod_foto;";
+			// 4.- Ejecutar la query.
+			$resultado=mysqli_query($conexion, $consulta);
+			// 5.- Comprobar.
+			//Si hay algun error, lo muestro en caso contrario continuo con el WHILE.
+			if(mysqli_errno($conexion)!=0){
+				//Quiere decir que hay un error.
+				echo "<p>No error: ".mysqli_errno($conexion)."</p>";
+				echo "<p>Mensaje error: ".mysqli_error($conexion)."</p>";
+			} else{
+				//Quiere decir que no hay error.
+			}
+			// 6.- Mostrar datos por pantalla (Si es un query SELECT)
+
+			while($dato=mysqli_fetch_array($resultado)){
+				//echo "<p class='negrita'>".$dato["nombre"]." ".$dato["apellidos"]."</p>";
+
+				echo "<img src='../img/reformas/$dato[nombre]'>";
+			} 	
+		}
+	// 7.- Cerramos la conexion.
+	mysqli_close($conexion);
+	}
+	/* ?> cierra el interprete.*/
+	?>
+</section>
+</body>
+</html>
+
+
+
